7f45bdd02e0562e3395da5695d7ea14e6251647e
Hussein Najah <husseinnajah123@gmail.com>
2024-05-17 17:57:42 +0300

    main

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Hero from "@/components/hero";
 
 export default function Home() {
   return (
-    <main className="relative">
-      <div>
-        <h1>Hello Portfolio</h1>
+    <main className="bg-black-100 sm:px-10 relative flex flex-col items-center justify-center px-5 mx-auto overflow-hidden">
+      <div className="max-w-7xl w-full">
+        <Hero />
       </div>
     </main>
   );
