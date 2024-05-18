9f5f621021351d9a3460908300493221d4924e72
Hussein Najah <husseinnajah123@gmail.com>
2024-05-18 14:43:10 +0300

    Main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+import Grid from "@/components/Grid";
 import Hero from "@/components/hero";
+import { FloatingNav } from "@/components/ui/FloatingNav";
+import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="bg-black-100 sm:px-10 relative flex flex-col items-center justify-center px-5 mx-auto overflow-hidden">
       <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
         <Hero />
+        <Grid />
       </div>
     </main>
   );
