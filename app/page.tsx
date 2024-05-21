433d698bb9b0944279913754a74a31ad18132067
Hussein Najah <husseinnajah123@gmail.com>
2024-05-21 21:40:47 +0300

    page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,17 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/hero";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import RecentProjects from "@/components/RecentProjects";
 import { navItems } from "@/data";
 
 export default function Home() {
   return (
     <main className="bg-black-100 sm:px-10 relative flex flex-col items-center justify-center px-5 mx-auto overflow-hidden">
       <div className="max-w-7xl w-full">
-        <FloatingNav navItems={navItems} />
+        <FloatingNav navItems={navItems} className="max-w-[90vw]" />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   );
