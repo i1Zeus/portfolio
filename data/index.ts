0fb978cfac86abc8646e9fc99baaed01e80cea8b
Hussein Najah <husseinnajah123@gmail.com>
2024-05-22 18:43:50 +0300

    data

--- data/index.ts
+++ data/index.ts
@@ -176,22 +176,22 @@ export const workExperience = [
   {
     id: 1,
     title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    desc: "Assisted in the development of a web-based platform using Next.js, enhancing interactivity.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
+    title: "UI/UX Designer & Developer",
+    desc: "Designed and developed a responsive web app for a startup, focusing on user experience.",
+    className: "md:col-span-2",
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
+    title: "Freelance Web Dev Project",
+    desc: "Led the dev of a web app for a client, from initial concept to deployment.",
+    className: "md:col-span-2",
     thumbnail: "/exp3.svg",
   },
   {
