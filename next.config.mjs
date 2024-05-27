d2b95aa3f042ee4c26ffe9329d7b9b349f932165
Hussein Najah <husseinnajah123@gmail.com>
2024-05-27 21:50:16 +0300

    refactor: update image loader and path in next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
+  reactStrictMode: true,
   images: {
-    unoptimized: true,
+    loader: "akamai",
+    path: "/",
   },
 };
 
