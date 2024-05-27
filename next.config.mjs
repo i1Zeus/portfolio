419a59cce77072412738a453f172346453de77e3
Hussein Najah <husseinnajah123@gmail.com>
2024-05-27 20:58:10 +0300

    base path agin

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   basePath: "/portfolio",
   assetPrefix: "/portfolio/",
@@ -7,4 +8,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
