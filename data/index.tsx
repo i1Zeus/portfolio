9cfc56b932305654e54149b6155f8f82dd08a561
Hussein Najah <husseinnajah123@gmail.com>
2024-06-21 12:51:24 +0300

    Finall Commit

--- data/index.tsx
+++ data/index.tsx
@@ -38,7 +38,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/section/b1.svg",
+    img: "./section/b1.svg",
     spareImg: "",
   },
   {
@@ -69,7 +69,7 @@ export const gridItems = [
     imgClassName: "",
     titleClassName: "justify-start",
     img: "/grid.svg",
-    spareImg: "/section/b4.svg",
+    spareImg: "./section/b4.svg",
   },
   {
     id: 5,
@@ -78,7 +78,7 @@ export const gridItems = [
     className: "md:col-span-3 md:row-span-2",
     imgClassName: "absolute right-0 bottom-0 md:w-[500px] w-60",
     titleClassName: "justify-center md:justify-start lg:justify-center",
-    img: "/section/b5.svg",
+    img: "./section/b5.svg",
     spareImg: "/grid.svg",
   },
   {
